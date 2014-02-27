71536733081086d95b1b892e57fefbaa3ef483d0
Admin <admin@Andreas-iMac-2.local>
2014-02-27 14:28:46 +0100

    Initial Commit

---
+++ MagiaTests/MagiaTests.m
@@ -0,0 +1,34 @@
+//
+//  MagiaTests.m
+//  MagiaTests
+//
+//  Created by Admin on 27/02/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MagiaTests : XCTestCase
+
+@end
+
+@implementation MagiaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
